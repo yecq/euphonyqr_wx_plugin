ba265dc1960c9bfa03f4beed6af9c0ae278079ee
yecq <yecq_82@hotmail.com>
2019-12-03 14:29:23 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//app.js
+App({
+  detector: requirePlugin("euphonyqr"),
+  onLaunch: function () {
+    
+    var config = this.detector.getConfig();//此方法返回默认config
+    console.log(config);
+    this.detector.init({
+      //这只是个demo,请联系动听获取appkey,同时布署自己的buyfull token service
+      appKey: "75ba120532f44aa7a8cd431a2c2a50ef",
+      buyfullTokenUrl: "https://sandbox.euphonyqr.com/testycq2/buyfulltoken",
+      // abortTimeout: 3000,//单次网络请求超时
+      //detectTimeout: 6000,//总超时
+      // debugLog: true,//true可以打开debugLog
+      //channelMask: (this.detector.channelMask.CHANNEL_A | this.detector.channelMask.CHANNEL_B | this.detector.channelMask.CHANNEL_C | this.detector.channelMask.CHANNEL_D), //可以选择是否只针对4信道中的某几个录音
+      //limitDB: -125,//当手机录音的分贝数低于此值时不上传检测
+      // pauseAfterDetect: false, //检测完成后是否停止录音，设为false可提高检测速度，适合需要一直检测的场景
+      // startBoost: false,//第一次检测时加速，适合只需要检测一次成功值的场景
+      //retryBeforeTimeout: true,//如果没有检测结果，在没有超时前，自动重试
+    });
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,71 @@
+var app = getApp()
+var detector = app.detector //请查看app.js
+Page({
+  reqid: null,
+  onLoad: function () {
+
+  },
+  onHide: function () {
+    detector.stop();//如果init时设置了pauseAfterDetect = false，则需要停止录音
+  },
+  onclick3: function () {
+    detector.stop();//如果init时设置了pauseAfterDetect = false，则需要停止录音
+  },
+  onclick2: function () {
+    if (this.reqid){
+      detector.debugUpload(this.reqid);//可以上传给动听工作人员用来确认BUG
+    }
+  },
+  onclick: function () {
+    if (detector.isDetecting()) {
+      return;//如果正在检测中，请等结束后再调用
+    }
+    this.doDetect();
+  },
+
+  doDetect: function () {
+    var thiz = this;
+    detector.detect({
+      version: "v2", //针对qieshu.net上的帐号请使用v2
+      openID: "", //可选，字符串，微信后台返回
+      unionID: "", //可选，字符串，微信后台返回
+      phoneNumber: "", //可选，字符串，openid,unionid,phonenumber中可以任意给一个或多个，可以给后台报表数据中作为统计数据的一个参数,没有将无法获得后台统计数据
+      customData: ""//,可选，字符串，可以给后台报表数据中作为统计数据回传
+    }, function (result) {
+      console.log("检测结束,结果是:" + JSON.stringify(result));
+      thiz.reqid = result.reqid;
+      if (result.count > 0) {
+        wx.showModal({
+          title: 'result is:',
+          content: JSON.stringify(result.allTags),
+        })
+      } else {
+        wx.showModal({
+          title: 'result is null, power is (dB):',
+          content: result.sortByPowerResult[0].power + "|" + result.sortByPowerResult[1].power,
+        })
+      }
+    }, function (errorCode, errorMsg) {
+      //检测有错误会回调
+      //errorcode 定义请查看 "buyfullsdk.js"
+      console.error(errorMsg);
+      if (errorCode == detector.error.NO_RECORD_PERMISSION) {
+        //没有录音权限，提醒打开权限
+        wx.authorize({
+          scope: 'scope.record',
+          success: function(res2){
+            console.log(res2);
+            thiz.doDetect();
+          },fail: function(err){
+            console.error(err);
+          }
+        })
+        return;
+      }
+      //其它错误都可以重试
+      setTimeout(function(){
+        thiz.doDetect();
+      },1000);
+    });
+  },
+})
